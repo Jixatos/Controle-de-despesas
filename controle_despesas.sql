f735682cbb83cb8a256641658a9e135fd09e2332
Juan <126682817+Jixatos@users.noreply.github.com>
2023-09-13 20:40:50 -0300

    Update and rename CP1_RM551408.SQL.sql to controle_despesas.sql

--- controle_despesas.sql
+++ controle_despesas.sql
@@ -29,8 +29,8 @@ CREATE TABLE tbl_fornecedores_rm551408 (
 );
 
 /*
-O atributo de complemento no diagrama está como NOT NULL, porém, nas planilhas não há alguns dos fornecedores que não apresentaram complemento sendo assim, irei deixar
-o atributo como NULLABLE para ser possível deixar nulo.
+O atributo de complemento no diagrama estÃ¡ como NOT NULL, porÃ©m, nas planilhas nÃ£o hÃ¡ alguns dos fornecedores que nÃ£o apresentaram complemento sendo assim, irei deixar
+o atributo como NULLABLE para ser possÃ­vel deixar nulo.
 */
 
 CREATE TABLE tbl_tipo_telefones_rm551408 (
@@ -51,8 +51,8 @@ CREATE TABLE tbl_telefones_rm551408 (
 );
 
 /*
-O atributo numero estava como NOT NULL no diagrama, porém, na tabela um dos fornecedores não possui um número de telefone, logo, o valor entra como nulo.
-Como houve um erro entre os dados fornecidos com o diagrama, irei deixar a coluna de número NULLABLE
+O atributo numero estava como NOT NULL no diagrama, porÃ©m, na tabela um dos fornecedores nÃ£o possui um nÃºmero de telefone, logo, o valor entra como nulo.
+Como houve um erro entre os dados fornecidos com o diagrama, irei deixar a coluna de nÃºmero NULLABLE
 */
 
 CREATE TABLE tbl_categorias_rm551408 (
@@ -78,9 +78,9 @@ CREATE TABLE tbl_despesas_rm551408 (
     FOREIGN KEY (id_categoria) REFERENCES tbl_categorias_rm551408(id_categoria)
 );
 
-/*1ª Exercício: inserir os valores que foram dados na documentação, no formato de tabela.*/
+/*1Âª ExercÃ­cio: inserir os valores que foram dados na documentaÃ§Ã£o, no formato de tabela.*/
     /*Tabela - Estados*/
-INSERT INTO tbl_estados_rm551408 (sigla, nome_estado) VALUES('SP','São Paulo');
+INSERT INTO tbl_estados_rm551408 (sigla, nome_estado) VALUES('SP','SÃ£o Paulo');
 
 SELECT * FROM tbl_estados_rm551408;
 
@@ -92,23 +92,23 @@ SELECT * FROM tbl_tipo_telefones_rm551408;
 
     /*Tabela - Categorias*/
 INSERT INTO tbl_categorias_rm551408(id_categoria, descricao) VALUES (1, 'Ferramentas');
-INSERT INTO tbl_categorias_rm551408(id_categoria, descricao) VALUES (2, 'Serviços de T.I');
-INSERT INTO tbl_categorias_rm551408(id_categoria, descricao) VALUES (3, 'Serviços Contábeis');
+INSERT INTO tbl_categorias_rm551408(id_categoria, descricao) VALUES (2, 'ServiÃ§os de T.I');
+INSERT INTO tbl_categorias_rm551408(id_categoria, descricao) VALUES (3, 'ServiÃ§os ContÃ¡beis');
 INSERT INTO tbl_categorias_rm551408(id_categoria, descricao) VALUES (4, 'Diversos');
 
 SELECT * FROM tbl_categorias_rm551408;
 
     /*Tabela - Cidades*/
-INSERT INTO tbl_cidades_rm551408(id_cidade, nome_cidade, sigla) VALUES (1, 'São Paulo','SP');
+INSERT INTO tbl_cidades_rm551408(id_cidade, nome_cidade, sigla) VALUES (1, 'SÃ£o Paulo','SP');
 INSERT INTO tbl_cidades_rm551408(id_cidade, nome_cidade, sigla) VALUES (2, 'Campinas','SP');
-INSERT INTO tbl_cidades_rm551408(id_cidade, nome_cidade, sigla) VALUES (3, 'Mariporã','SP');
+INSERT INTO tbl_cidades_rm551408(id_cidade, nome_cidade, sigla) VALUES (3, 'MariporÃ£','SP');
 
 SELECT * FROM tbl_cidades_rm551408;
 
     /*Tabela - Fornecedores*/
 INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bairro, cep, id_cidade, dt_cadastro, email, status)
     VALUES(1,
-        'Ferramentaria do Josué Ltda',
+        'Ferramentaria do JosuÃ© Ltda',
         'Avenida Paulista',
         '1515', 
         'Paulista',
@@ -120,7 +120,7 @@ INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bai
         
 INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bairro, complemento, cep, id_cidade, dt_cadastro, email, status) 
     VALUES(2,
-        'Flix Tecnologia e Serviços Ltda',
+        'Flix Tecnologia e ServiÃ§os Ltda',
         'Rua Brasilia',
         '23',
         'Lapa',
@@ -133,8 +133,8 @@ INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bai
         
 INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bairro, cep, id_cidade, dt_cadastro, status)
     VALUES(3,
-        'Rei dos Descartáveis Ltda',
-        'Rua Voluntários da Pátria',
+        'Rei dos DescartÃ¡veis Ltda',
+        'Rua VoluntÃ¡rios da PÃ¡tria',
         '356',
         'Santana',
         '01106012',
@@ -144,7 +144,7 @@ INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bai
         
 INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bairro, cep, id_cidade, dt_cadastro, status)
     VALUES(4,
-        'Manut Serviços de manutenção Ltda',
+        'Manut ServiÃ§os de manutenÃ§Ã£o Ltda',
         'Avenida Aquidaban',
         '2000',
         'Centro',
@@ -154,7 +154,7 @@ INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bai
         'ATIVO');
         
 INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bairro, cep, id_cidade, dt_cadastro, status)
-    VALUES(5, 'Serviços de Jardinagem Ltda',
+    VALUES(5, 'ServiÃ§os de Jardinagem Ltda',
         'Rua Campinas',
         '15',
         'Centro',
@@ -165,8 +165,8 @@ INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bai
         
 INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bairro, complemento, cep, id_cidade, dt_cadastro, email)
     VALUES(6,
-        'ES Serviços Contábeis Ltda',
-        'Avenida Pompéia',
+        'ES ServiÃ§os ContÃ¡beis Ltda',
+        'Avenida PompÃ©ia',
         '1500',
         'Lapa',
         'Sala 10',
@@ -291,17 +291,17 @@ INSERT INTO tbl_despesas_rm551408
         
 SELECT * FROM tbl_despesas_rm551408;
 
-/*2.) Efetue as seguintes alterações nas estruturas das tabelas*/ 
+/*2.) Efetue as seguintes alteraÃ§Ãµes nas estruturas das tabelas*/ 
     /*Tabela - Fornecedores*/
-    /*a.) Valor padrão no campo dt_cadastro para a data atual*/
+    /*a.) Valor padrÃ£o no campo dt_cadastro para a data atual*/
 ALTER TABLE tbl_fornecedores_rm551408 MODIFY(dt_cadastro DEFAULT SYSDATE);
-    /*b.) Campo de Status deve ser obrigatório e aceitar somente os valores ATIVO e INATIVO*/
+    /*b.) Campo de Status deve ser obrigatÃ³rio e aceitar somente os valores ATIVO e INATIVO*/
 UPDATE tbl_fornecedores_rm551408 SET status = 'INATIVO' WHERE status IS NULL;
 SELECT * FROM tbl_fornecedores_rm551408;
 ALTER TABLE tbl_fornecedores_rm551408 ADD CONSTRAINT status_ativo_ou_inativo_CK CHECK (status IN('ATIVO', 'INATIVO'));
 
     /*Tabela - Despesas*/
-    /*c.) Valor padrão no campo dt_cadastro para a data atual*/
+    /*c.) Valor padrÃ£o no campo dt_cadastro para a data atual*/
 ALTER TABLE tbl_despesas_rm551408 MODIFY(dt_cadastro DEFAULT SYSDATE);
 
     /*d.) A data de vencimento deve ser superior ou igual a data de cadastro*/
@@ -310,58 +310,58 @@ ALTER TABLE tbl_despesas_rm551408 ADD CONSTRAINT dt_vencimento_maior_ou_igual_dt
     /*e.) Altere o nome do atributo dt_cadastro para dt_lancamento*/
 ALTER TABLE tbl_despesas_rm551408 RENAME COLUMN dt_cadastro to dt_lancamento;
 
-    /*f.) Adicione um atributo novo com a definição de situação em que os valores aceitos devem ser PENDENTE, PAGO, EM ATRASO*/
+    /*f.) Adicione um atributo novo com a definiÃ§Ã£o de situaÃ§Ã£o em que os valores aceitos devem ser PENDENTE, PAGO, EM ATRASO*/
 ALTER TABLE tbl_despesas_rm551408 ADD situacao VARCHAR(9) CONSTRAINT situacao_pendente_pago_ou_atraso_CK CHECK (situacao IN('PENDENTE','PAGO','EM ATRASO'));
 
-    /*g.) Adicione comentários em todos os atributos para melhorar a documentação;*/
+    /*g.) Adicione comentÃ¡rios em todos os atributos para melhorar a documentaÃ§Ã£o;*/
 /*(id_despesas, dt_cadastro, id_fornecedor, numero_docto, parcela_atual, parcela_maxima, dt_vencimento, valor, valor_pago, id_categoria, dt_pagamento)*/
-COMMENT ON COLUMN tbl_despesas_rm551408.id_despesas IS 'Identificação da despesa';
+COMMENT ON COLUMN tbl_despesas_rm551408.id_despesas IS 'IdentificaÃ§Ã£o da despesa';
 COMMENT ON COLUMN tbl_despesas_rm551408.dt_cadastro IS 'Data atual que foi cadastrato a despesa';
-COMMENT ON COLUMN tbl_despesas_rm551408.id_fornecedor IS 'Identificação do fornecedor';
-COMMENT ON COLUMN tbl_despesas_rm551408.numero_docto IS 'Número do documento';
-COMMENT ON COLUMN tbl_despesas_rm551408.parcela_atual IS 'Parcela que atualmente está sendo paga ou foi paga.';
-COMMENT ON COLUMN tbl_despesas_rm551408.parcela_maxima IS 'Parcela máxima quue precisa ser paga';
+COMMENT ON COLUMN tbl_despesas_rm551408.id_fornecedor IS 'IdentificaÃ§Ã£o do fornecedor';
+COMMENT ON COLUMN tbl_despesas_rm551408.numero_docto IS 'NÃºmero do documento';
+COMMENT ON COLUMN tbl_despesas_rm551408.parcela_atual IS 'Parcela que atualmente estÃ¡ sendo paga ou foi paga.';
+COMMENT ON COLUMN tbl_despesas_rm551408.parcela_maxima IS 'Parcela mÃ¡xima quue precisa ser paga';
 COMMENT ON COLUMN tbl_despesas_rm551408.dt_vencimento IS 'Data de vencimento da parcela atual';
 COMMENT ON COLUMN tbl_despesas_rm551408.valor IS 'Valor total a ser pagado, divido entre as parcelas';
 COMMENT ON COLUMN tbl_despesas_rm551408.valor_pago IS 'Valor total pago';
-COMMENT ON COLUMN tbl_despesas_rm551408.id_categoria IS 'Identificação da categoria que o fornecedor se enquadra';
-COMMENT ON COLUMN tbl_despesas_rm551408.dt_pagamento IS 'Data, da qual, ocorrou o pagamento da última parcela';
+COMMENT ON COLUMN tbl_despesas_rm551408.id_categoria IS 'IdentificaÃ§Ã£o da categoria que o fornecedor se enquadra';
+COMMENT ON COLUMN tbl_despesas_rm551408.dt_pagamento IS 'Data, da qual, ocorrou o pagamento da Ãºltima parcela';
 
-    /*h.) O valor das despesas deve ser obrigatório e maior que ZERO*/
+    /*h.) O valor das despesas deve ser obrigatÃ³rio e maior que ZERO*/
 ALTER TABLE tbl_despesas_rm551408 ADD CONSTRAINT valor_maior_que_zero_CK CHECK (valor > 0);
 
-    /*i.) O valor de parcela_atual não deve ser maior que o parcela_maxima*/
+    /*i.) O valor de parcela_atual nÃ£o deve ser maior que o parcela_maxima*/
 ALTER TABLE tbl_despesas_rm551408 ADD CONSTRAINT parcela_atual_menor_parcela_maxima_CK CHECK (parcela_atual <= parcela_maxima);
 
-/*3.) Atualize os registros conforme as solicitações*/
-    /*a.) Houve uma alteração de preço dos serviços do fornecedor ES Serviços Contábeis Ltda.
-A partir de 01/01/2024 em 10% calcule o valor e atualize as despesas após essa data*/
+/*3.) Atualize os registros conforme as solicitaÃ§Ãµes*/
+    /*a.) Houve uma alteraÃ§Ã£o de preÃ§o dos serviÃ§os do fornecedor ES ServiÃ§os ContÃ¡beis Ltda.
+A partir de 01/01/2024 em 10% calcule o valor e atualize as despesas apÃ³s essa data*/
 SELECT * FROM tbl_fornecedores_rm551408;
 SELECT * FROM tbl_despesas_rm551408;
 UPDATE tbl_despesas_rm551408
 SET valor = valor * 1.10 
 WHERE id_fornecedor = 1 AND SYSDATE > TO_DATE('2024-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS');
 
-    /*b.) Atualize o nome do fornecedor “Manut Serviços de manutenção Ltda” para “Serviços de Manutenção Ltda”;*/
-UPDATE tbl_fornecedores_rm551408 SET nome = 'Serviços de Manutenção Ltda' WHERE nome = 'Manut Serviços de manutenção Ltda';
+    /*b.) Atualize o nome do fornecedor Â“Manut ServiÃ§os de manutenÃ§Ã£o LtdaÂ” para Â“ServiÃ§os de ManutenÃ§Ã£o LtdaÂ”;*/
+UPDATE tbl_fornecedores_rm551408 SET nome = 'ServiÃ§os de ManutenÃ§Ã£o Ltda' WHERE nome = 'Manut ServiÃ§os de manutenÃ§Ã£o Ltda';
 
-    /*c.) Inative todos os fornecedores que não estão na cidade de São Paulo*/
+    /*c.) Inative todos os fornecedores que nÃ£o estÃ£o na cidade de SÃ£o Paulo*/
 UPDATE tbl_fornecedores_rm551408 SET status = 'INATIVO' WHERE (id_cidade <> 1);
 
     /*d.) Adicione o seguinte fornecedor:
-            Hotel Manjubinha – Congressos e Convenções Ltda.
-            Rua Beira Rio, 1655 – Praia Sul
-            Balneário Camboriú – Santa Catarina
+            Hotel Manjubinha Â– Congressos e ConvenÃ§Ãµes Ltda.
+            Rua Beira Rio, 1655 Â– Praia Sul
+            BalneÃ¡rio CamboriÃº Â– Santa Catarina
             CEP 22333-333
             Telefone (54) 6598-9898
 */
 INSERT INTO tbl_estados_rm551408 (sigla, nome_estado) VALUES('SC','Santa Catarina');
 
-INSERT INTO tbl_cidades_rm551408(id_cidade, nome_cidade, sigla) VALUES (4, 'Balneário Camboriú','SC');
+INSERT INTO tbl_cidades_rm551408(id_cidade, nome_cidade, sigla) VALUES (4, 'BalneÃ¡rio CamboriÃº','SC');
 
 INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bairro, cep, id_cidade)
     VALUES(7,
-        'Hotel Manjubinha – Congressos e Convenções Ltda.',
+        'Hotel Manjubinha Â– Congressos e ConvenÃ§Ãµes Ltda.',
         'Rua Beira Rio',
         '1655',
         'Praia Sul',
@@ -371,11 +371,11 @@ INSERT INTO tbl_fornecedores_rm551408(id_fornecedor, nome, endereco, numero, bai
 INSERT INTO tbl_telefones_rm551408(id_telefone, id_fornecedor, numero, tbl_tipo_telefones_rm551408_tbl_tipo_telefones_rm551408_id)
 VALUES(7, 7, '(54) 6598-9898', 1);
 
-        /*e.) Adicione uma nova categoria com a descrição: Congressos e Convenções;*/
+        /*e.) Adicione uma nova categoria com a descriÃ§Ã£o: Congressos e ConvenÃ§Ãµes;*/
 INSERT INTO tbl_categorias_rm551408 (id_categoria, descricao)
-VALUES(5, 'Congressos e Convenções');
+VALUES(5, 'Congressos e ConvenÃ§Ãµes');
 
-        /*f.) Adicione a despesas no valor R$ 500,00 para o Hotel Manjubinha – Congressos e Convenções Ltda, na categoria Congressos e Convenções;*/
+        /*f.) Adicione a despesas no valor R$ 500,00 para o Hotel Manjubinha Â– Congressos e ConvenÃ§Ãµes Ltda, na categoria Congressos e ConvenÃ§Ãµes;*/
 SELECT * FROM tbl_despesas_rm551408;
 INSERT INTO tbl_despesas_rm551408 (id_despesas, id_fornecedor, numero_docto, parcela_atual, parcela_maxima, dt_vencimento, valor, id_categoria)
 VALUES (8,
@@ -387,10 +387,10 @@ VALUES (8,
     500.00,
     5);
 
-        /*g.) Atualize o campo de status que foi adicionado na tabela tbl_despesas_RMxxxxx conforme a condição abaixo:
+        /*g.) Atualize o campo de status que foi adicionado na tabela tbl_despesas_RMxxxxx conforme a condiÃ§Ã£o abaixo:
 Todos os registros sem data de pagamento e a data atual maior que o vencimento deve ficar como PENDENTE;
 Todos os registros com data de pagamento e valor de pagamento preenchido atualizar para PAGO;
-Todos os registros sem informações de pagamento e data de vencimento menor que hoje: EM ATRASO;
+Todos os registros sem informaÃ§Ãµes de pagamento e data de vencimento menor que hoje: EM ATRASO;
 */
 SELECT * FROM tbl_despesas_rm551408;
 UPDATE tbl_despesas_rm551408 SET situacao = 'PENDENTE' WHERE (dt_pagamento IS NULL AND SYSDATE > dt_vencimento);
